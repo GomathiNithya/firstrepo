d24d0d935386285e3b7f4e86cb241c721081408d
GomathiNithya <35761884+GomathiNithya@users.noreply.github.com>
2018-02-06 11:32:18 +0530

    committed

--- frombitbucket.py
+++ frombitbucket.py
@@ -14,7 +14,7 @@ regions=['us-east-1','us-east-2','us-west-1','us-west-2','eu-west-1','eu-west-2'
          'ca-central-1','eu-central-1','ap-northeast-1','ap-northeast-2','ap-southeast-1',
          'ap-southeast-2','ap-south-1','sa-east-1'];
 
-
+#regions
 #regionsName
 
 regionsName=['US East\n(N. Virginia)\t','US East (Ohio)','US West \n(N.California)\t','US West (Oregon)','EU (Ireland)','EU (London)','EU (Paris)',
